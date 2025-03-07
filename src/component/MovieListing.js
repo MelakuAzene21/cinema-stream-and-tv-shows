eeafee9ab06655274e67f004fe8604327f812ebf
Melaku Azene <137656530+MelakuAzene21@users.noreply.github.com>
2025-03-07 20:25:52 +0300

    feat:remove unneccssary code

--- src/component/MovieListing.js
+++ src/component/MovieListing.js
@@ -1,30 +1,3 @@
-// import MovieCard from "./MovieCard";
-// import "./MovieListing.css";
-
-// export const MovieListing = ({ movies, loading }) => {
-//   return (
-//     <div className="movie-wrapper">
-//       <div className="movie-list">
-//         <h2>MOVIES</h2>
-//         <div className="movie-container">
-//           {loading ? (
-//             [...Array(10)].map((_, index) => (
-//               <div key={index} className="skeleton-loader"></div>
-//             ))
-//           ) : movies && movies.length ? (
-//             movies.map((movie) => <MovieCard key={movie.id} data={movie} />)
-//           ) : (
-//             <h3>No movies found</h3>
-//           )}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default MovieListing;
-
-
 import MovieCard from "./MovieCard";
 import CastCrew from "./CastCrew";  // Import Cast & Crew Component
 import "./MovieListing.css";
--- src/component/MovieModal.js
+++ src/component/MovieModal.js
@@ -1,32 +1,3 @@
-// // MovieModal.js
-// import React, { useState } from 'react';
-// import './MovieModal.css';
-
-// const MovieModal = ({ trailerUrl, isOpen, closeModal }) => {
-//     if (!isOpen) return null;
-
-//     return (
-//         <div className="modal-overlay" onClick={closeModal}>
-//             <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-//                 <button className="close-btn" onClick={closeModal}>
-//                     &times;
-//                 </button>
-//                 <iframe
-//                     width="100%"
-//                     height="400px"
-//                     src={trailerUrl}
-//                     title="Movie Trailer"
-//                     frameBorder="0"
-//                     allowFullScreen
-//                 ></iframe>
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default MovieModal;
-
-
 import React, { useEffect } from "react";
 import "./MovieModal.css";
 
